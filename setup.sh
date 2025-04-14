2354da8553eaec5463267505b54c91018af2ba98
Mohammad Hamid <mohammad200h@hotmail.com>
2025-04-14 22:11:40 +0100

    Update setup.sh
    Fixed a bug

--- setup.sh
+++ setup.sh
@@ -3,13 +3,13 @@ git clone git@github.com:mohammad200h/image_viewer_foundation_pose.git && \
 git clone git@github.com:mohammad200h/foundation_pose_interfaces.git && \
 git clone git@github.com:mohammad200h/foundation_pose.git
 
-
+cd ../..
 mkdir g_sam_ws && mkdir g_sam_ws/src && cd g_sam_ws/src && \
 git clone git@github.com:mohammad200h/image_viewer_g_sam.git && \
 git clone git@github.com:mohammad200h/g_sam.git && \
 git clone git@github.com:mohammad200h/fake_realsense.git && \
 git clone git@github.com:mohammad200h/fake_g_sam.git
 
-
+cd ../..
 mkdir realsense_ws && mkdir realsense_ws/src && cd realsense_ws/src && \
 git clone git@github.com:mohammad200h/realsense-ros.git
